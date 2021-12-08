2ec6d9f2b94471dbe285bedb7547ff44a626c8e8
bwalters13 <34872925+bwalters13@users.noreply.github.com>
2021-12-08 15:24:14 -0800

    update navbar

--- my-app/src/navbar.jsx
+++ my-app/src/navbar.jsx
@@ -5,6 +5,10 @@ import living from './liv.jpg'
 
 function Navbar() {
   return (
+    <div style={{width: "100%"}}>
+      <div class="top">
+        <img src={living} alt="" />
+      </div>
     <div class="navbar">  
       {/* <img src={living} alt="" /> */}
       <li>
@@ -13,6 +17,7 @@ function Navbar() {
       <li>
         <Link to="/scores">Matchups</Link>
       </li>
+    </div>
     </div>
   )
 }
